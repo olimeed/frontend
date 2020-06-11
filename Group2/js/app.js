a8884e37d0b3f21e752797a8885f2e7db5ce5432
XodTuza <gordie2u@gmail.com>
2020-06-11 23:44:51 +0100

    nav menu

--- Group2/js/app.js
+++ Group2/js/app.js
@@ -1,6 +1,8 @@
-$(document).ready(function() {
+console.log("demo");
+
+$(document).ready(function () {
     // Show or hide the sticky footer button
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         if ($(this).scrollTop() > 200) {
             $('.go-top').fadeIn(200);
         } else {
@@ -9,9 +11,33 @@ $(document).ready(function() {
     });
 
     // Animate the scroll to top
-    $('.go-top').click(function(event) {
+    $('.go-top').click(function (event) {
         event.preventDefault();
 
-        $('html, body').animate({ scrollTop: 0 }, 300);
+        $('html, body').animate({scrollTop: 0}, 300);
     })
 });
+
+
+// Hamburger JS
+const ham = document.querySelector(".hamburger");
+const ham1 = document.querySelector(".ham1");
+const ham2 = document.querySelector(".ham2");
+const ham3 = document.querySelector(".ham3");
+const navbar = document.querySelector(".Mobile-menu");
+const body = document.querySelector("html");
+
+const menuOpen = e => {
+    ham1.classList.toggle("ham-tilt-right");
+    ham2.classList.toggle("ham-no-show");
+    ham3.classList.toggle("ham-tilt-left");
+    navbar.classList.toggle("nav-out");
+    navbar.classList.toggle("MMmenu");
+    body.classList.toggle("no-scroll");
+
+    console.log("work")
+};
+
+ham.addEventListener("click", menuOpen);
+
+
